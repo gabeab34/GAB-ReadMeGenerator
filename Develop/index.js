5082ced302f92aca3cd651030dc6faf99b8f0282
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-07-05 20:20:02 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,94 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+
+        type: 'input',
+        name: 'title',
+        message: 'What do you want your project to be named?'
+
+    },
+    {
+
+        type: 'input',
+        name: 'description',
+        message: 'Please briefly describe your project'
+
+    },
+    {
+
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter any steps needed to install your project'
+
+    },
+    {
+
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any needed usage information for your project'
+
+    },
+    {
+
+        type: 'input',
+        name: 'contributions',
+        message: 'Please briefly state how other developers can contribute to your project'
+
+    },
+    {
+
+        type: 'input',
+        name: 'test',
+        message: 'Please give testing instructions for your project'
+
+    },
+    {
+
+        type: 'input',
+        name: 'email',
+        message: 'please enter your email address'
+
+    },
+    {
+
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github username'
+
+    },
+    {
+
+        type: 'input',
+        name: 'licenses',
+        message: 'Please enter any licenses needed for your projcet'
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
